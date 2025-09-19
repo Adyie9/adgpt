06d267b9a3cbeb6620a761fb61430549a86f4965
Durba Banerjee <114578740+Adyie9@users.noreply.github.com>
2025-09-19 16:39:17 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://adgpts.vercel.app/login", // ✅ only /api
+  baseURL: "https://adgpts.vercel.app/api", // ✅ only /api
   withCredentials: true
 });
 
