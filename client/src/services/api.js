2fe64fb46b6770f5140ac6c098b7168fb23998d8
Durba Banerjee <114578740+Adyie9@users.noreply.github.com>
2025-09-19 14:00:55 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ only /api
+  baseURL: "https://adgpt-backend.onrender.com", // ✅ only /api
   withCredentials: true
 });
 
